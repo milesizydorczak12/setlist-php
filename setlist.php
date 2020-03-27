707f7ab25dfaceb736d7370176196b565a7b8f2c
Miles Izydorczak <56458475+milesizydorczak12@users.noreply.github.com>
2020-03-26 21:29:11 -0400

    Add files via upload

---
+++ setlist.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<?php 
+echo '[
+    {
+        "Title" : "Everlong",
+        "Artist(s)" : [ 
+            "Foo Fighters"
+        ],
+        "Genre(s)" : [
+            "Post-grunge",
+            "Alternative Rock",
+            "Hard Rock",
+            "Indie Rock"
+        ],
+        "Year" : "1997"
+    },
+
+    {
+        "Title" : "Its Been Awhile",
+        "Artist(s)" : [ 
+            "Staind"
+        ],
+        "Genre(s)" : [
+            "Post-grunge"
+        ],
+        "Year" : "2001"
+    },
+
+    {
+        "Title" : "I Hate Everything About You",
+        "Artist(s)" : [ 
+            "Three Days Grace"
+        ],
+        "Genre(s)" : [
+            "Post-grunge",
+            "Nu Metal"
+        ],
+        "Year" : "2003"
+    },
+
+    {
+        "Title" : "One Step Closer",
+        "Artist(s)" : [ 
+            "Linkin Park"
+        ],
+        "Genre(s)" : [
+            "Nu Metal"
+        ],
+        "Year" : "2000"
+    },
+
+    {
+        "Title" : "B.Y.O.B.",
+        "Artist(s)" : [ 
+            "System of a Down"
+        ],
+        "Genre(s)" : [
+            "Thrash Metal",
+            "Alternative Rock"
+        ],
+        "Year" : "2005"
+    },
+
+    {
+        "Title" : "Uprising",
+        "Artist(s)" : [ 
+            "Muse"
+        ],
+        "Genre(s)" : [
+            "Synth Rock",
+            "Alternative Rock",
+            "Glam Rock"
+        ],
+        "Year" : "2009"
+    },
+
+    {
+        "Title" : "Losing My Religion",
+        "Artist(s)" : [ 
+            "R.E.M."
+        ],
+        "Genre(s)" : [
+            "Alternative Rock",
+            "Folk Rock"
+        ],
+        "Year" : "1991"
+    }
+
+    {
+        "Title" : "Losing My Religion",
+        "Artist(s)" : [ 
+            "R.E.M."
+        ],
+        "Genre(s)" : [
+            "Alternative Rock",
+            "Folk Rock"
+        ],
+        "Year" : "1991"
+    }
+
+    {
+        "Title" : "Push",
+        "Artist(s)" : [ 
+            "Matchbox Twenty"
+        ],
+        "Genre(s)" : [
+            "Post-grunge"
+        ],
+        "Year" : "1997"
+    }
+
+    {
+        "Title" : "Here to Stay",
+        "Artist(s)" : [ 
+            "Korn"
+        ],
+        "Genre(s)" : [
+            "Nu metal"
+        ],
+        "Year" : "2002"
+    }
+]';
+?>
